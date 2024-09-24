dfcc6d55626fc45620e5876175d74302c29d3b4a
13-arch <Iron_Mike@tutamail.com>
2024-09-24 13:05:29 +0300

    sss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("hello")
-#команда принn
-"""
-hhhh
-"""
+input_data = open('input.txt','r')
+data = input_data.read()
+K = int(data)
+out = K * 100 + 90 +(9 - K)
+output_data = open('output.txt','w')
+output_data.write(str(out))
+input_data.close()
+output_data.close()
